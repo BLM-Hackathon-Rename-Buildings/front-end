c9937edebd5dd0e29ffe04b38cfd938d2f25ee4d
Sara Laffin <sarala@umich.edu>
2020-06-13 19:56:46 -0400

    add logo and search to header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import './Header.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./Header.css";
 
 export default function App() {
   return (
-    <div>
-      <nav className="home-nav">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/add">Add Building</Link>
-          </li>
-
-          {/* I think this map link should only be accessed by a search bar on the home page or something, but let me know your thoughts! I've included it for now for testing  */}
-
-          <li>
-            <Link to="/map">Map</Link>
-          </li>
-        </ul>
-      </nav>
+    <div className="home-nav">
+      <Link to="/">
+        <div className="header-logo"></div>
+      </Link>
+      <div className="links-search">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/map">MAP</Link>
+            </li>
+            <li>
+              <Link to="/">ABOUT</Link>
+            </li>
+            <li>
+              <Link to="/add">ADD +</Link>
+            </li>
+          </ul>
+        </nav>
+        <form>
+          <input
+            type="text"
+            id="search"
+            name="search"
+            placeholder="SEARCH MAP"
+          />
+        </form>
+      </div>
     </div>
   );
 }
