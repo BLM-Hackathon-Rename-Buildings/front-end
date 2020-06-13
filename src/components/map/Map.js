bf68576dd7180488ff19d5ec24b6b31cde13a4b0
Carly Hagen <carlyhn@gmail.com>
2020-06-12 22:27:10 -0700

    greyscaled map, added test locations

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,15 +1,27 @@
 import React from "react";
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import { Icon } from "leaflet";
+import * as testData from "./testdata.json";
 import "./Map.css";
 
 export default function App() {
   return (
-    <Map center={[45.4, -75.7]} zoom={12}>
+    <Map center={[38, -96]} zoom={5}>
+      {testData.features.map(location => (
+        <Marker
+          key={location.properties.PARK_ID}
+          position={[
+            location.geometry.coordinates[1],
+            location.geometry.coordinates[0]
+          ]}
+        />
+      ))}
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
     </Map>
   );
 }
+
+//@carly to do: move this attribution somewhere else
+// attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
