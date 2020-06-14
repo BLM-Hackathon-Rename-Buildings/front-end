3378cd9653690fbaa17b767b77c3dd900c85426a
Carly Hagen <carlyhn@gmail.com>
2020-06-14 09:00:15 -0700

    styled about page

--- src/components/sidebar/SideBar.js
+++ src/components/sidebar/SideBar.js
@@ -28,13 +28,13 @@ export default class SideBar extends React.Component {
               </h2>
               <p className="symbol-info-field">
                 {' '}
-                This is dummy data. Not sure what we are going to put here but for
-                now this is just gibberish. I wonder how many lines this will have
-                on here. 'This is dummy data. Not sure what we are going to put here
-                but for now this is just gibberish. I wonder how many lines this
-                will have on here. 'This is dummy data. Not sure what we are going
-                to put here but for now this is just gibberish. I wonder how many
-                lines this will have on here.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+                ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat. Duis aute irure dolor in
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                culpa qui officia deserunt mollit anim id est laborum.
               </p>
             </div>
 
