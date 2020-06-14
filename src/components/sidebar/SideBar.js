7c433b81cf0cc5ce5ae81d20076bead504798add
Carly Hagen <carlyhn@gmail.com>
2020-06-13 19:15:51 -0700

    detail routing works

--- src/components/sidebar/SideBar.js
+++ src/components/sidebar/SideBar.js
@@ -2,24 +2,18 @@ import React from "react";
 import "./SideBar.css";
 
 export default class SideBar extends React.Component {
-  // constructor(props){
-  //   super(props);
-  //   this.state = {
-  //     currentMonument: "hello"
-  //   };
-  // }
+  constructor(props){
+    super(props);
+    console.log("param: " + this.props.match.params.id);
+  }
+
 
   render(){
     return (
-      <aside className="side-bar" />
+      <aside className="side-bar">{this.props.match.params.id}</aside>
     );
   }
 }
-
-// function Detail() {
-//   let { id } = this.useParams();
-//   return <aside>{id}</aside>;
-// }
 
 //@carly to do: move this attribution somewhere else
 // attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
