f63939bd75e28420e0ee9d023b863f04db100802
Carly Hagen <carlyhn@gmail.com>
2020-06-13 19:50:17 -0700

    replaced 'class' with 'className', worked fine but showed error in output

--- src/components/sidebar/SideBar.js
+++ src/components/sidebar/SideBar.js
@@ -15,19 +15,19 @@ export default class SideBar extends React.Component {
         <img
           src={this.props.currentMonumentPhoto}
           alt="symbol"
-          class="symbol-image"
+          className="symbol-image"
         />
-        <div class="symbol-info">
-          <div class="symbol-info-field">
+      <div className="symbol-info">
+          <div className="symbol-info-field">
             {' '}
             <b>Name:</b> {this.props.currentMonumentName}{' '}
           </div>
-          <div class="symbol-info-field">
+          <div className="symbol-info-field">
             {' '}
             <b>Type: </b>
             {this.props.currentMonumentType}{' '}
           </div>
-          <div class="symbol-info-field">
+          <div className="symbol-info-field">
             {' '}
             <b>Info:</b>
             This is dummy data. Not sure what we are going to put here but for
@@ -38,8 +38,8 @@ export default class SideBar extends React.Component {
             to put here but for now this is just gibberish. I wonder how many
             lines this will have on here.
           </div>
-          <div class="center-button">
-            <button class="remove-button">How to Get Statue Removed</button>
+          <div className="center-button">
+            <button className="remove-button">How to Get Statue Removed</button>
           </div>
         </div>
       </aside>
