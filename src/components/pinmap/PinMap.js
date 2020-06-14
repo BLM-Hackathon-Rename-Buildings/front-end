25127c0ea65d2c5145ea2257e11422a6b992abf0
Sara Laffin <sarala@umich.edu>
2020-06-14 13:39:56 -0400

    zoom into pin when clicked

--- src/components/pinmap/PinMap.js
+++ src/components/pinmap/PinMap.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
-import { Icon } from 'leaflet';
-import { connect } from 'react-redux';
-import * as testData from './testdata.json';
-import { useHistory } from 'react-router-dom'
-import './PinMap.css';
+import React from "react";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import { Icon } from "leaflet";
+import { connect } from "react-redux";
+import * as testData from "./testdata.json";
+import { useHistory } from "react-router-dom";
+import "./PinMap.css";
 
 class PinMapComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       selectedPin: null,
+      zoom: 5,
+      center: [38, -96],
     };
+  }
+
+  handleZoom(lat, long) {
+    this.setState({
+      zoom: 14,
+      center: [lat, long],
+    });
   }
 
   render() {
     return (
-      <Map center={[38, -96]} zoom={5}>
+      <Map center={this.state.center} zoom={this.state.zoom}>
         {testData.monuments.map((monument) => (
-          <MarkerButton key={monument.id} monument={monument}/>
+          <MarkerButton
+            key={monument.id}
+            monument={monument}
+            onPinClicked={this.handleZoom.bind(this)}
+          />
         ))}
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       </Map>
@@ -27,19 +40,20 @@ class PinMapComponent extends React.Component {
 }
 
 function MarkerButton(props) {
-    const history = useHistory();
+  const history = useHistory();
 
-    function handleClick(id) {
-      history.push("/detail/" + id);
-    }
-
-    return (
-      <Marker
-        position={[props.monument.latitude, props.monument.longitude]}
-        onClick={() => handleClick(props.monument.id) }
-      />
-    );
+  function handleClick(monument) {
+    history.push("/detail/" + monument.id);
+    props.onPinClicked(monument.latitude, monument.longitude);
   }
+
+  return (
+    <Marker
+      position={[props.monument.latitude, props.monument.longitude]}
+      onClick={() => handleClick(props.monument)}
+    />
+  );
+}
 
 function mapStateToProps(state) {
   return {
